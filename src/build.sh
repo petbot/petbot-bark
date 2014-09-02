f36beb27aed251da524a4f1cf52c02f5b86eea22
misko <misko@cs.toronto.edu>
2014-09-02 00:15:53 +0000

    build all

--- src/build.sh
+++ src/build.sh
@@ -1,4 +1,4 @@
 gcc -o listen_for_bark -lasound -lfftw3 -lm  -lrt ./listen_for_bark_gpu.c ./model.c -O3  -Wall  mailbox.c gpu_fft.c gpu_fft_twiddles.c gpu_fft_shaders.c -lpthread
-#gcc -o listen_for_bark -lasound -lfftw3 -lm ./listen_for_bark.c ./model.c -O3  -Wall
-#gcc -o run_model -lasound -lfftw3 -lm run_model.c ./model.c -Wall -O3 -Wall 
-#gcc -o capture -lasound -lfftw3 -lm ./capture.c -O3
+gcc -o listen_for_bark -lasound -lfftw3 -lm ./listen_for_bark.c ./model.c -O3  -Wall
+gcc -o run_model -lasound -lfftw3 -lm run_model.c ./model.c -Wall -O3 -Wall 
+gcc -o capture -lasound -lfftw3 -lm ./capture.c -O3
